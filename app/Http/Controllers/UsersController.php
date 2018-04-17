0168f09b7d3f1e8cd5c997124251dda1f9bcf6fd
zhangshuai <zs1121323952@gmail.com>
2018-04-17 04:13:32 +0000

    Add password resets & email configuration

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -93,14 +93,12 @@ class UsersController extends Controller
     protected function sendEmailConfirmationTo($user)
     {
         $view = 'emails.confirm';
-        $date = compact('user');
-        $from = 'zs1121323952@gmail.com';
-        $name = 'zhangshuai';
-        $to   = $user->email;
-        $subject = '感谢注册 Sina 应用！请确认你的邮箱。';
+        $data = compact('user');
+        $to = $user->email;
+        $subject = "感谢注册 Sina 应用！请确认你的邮箱。";
 
-        Mail::send($view,$date,function($message) use( $from,$name,$to,$subject){
-            $message->from($from,$name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 
